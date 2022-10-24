ade2e304767d2ac5c4f0cafa5f875f1c5ce7f0b3
Jahid1604091 <jh409780@gmail.com>
2022-10-24 07:42:44 +0600

    seeder

---
+++ backend/seeder.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const connectDB = require('./config/db');
+
+const users = require('./data/users');
+const products = require('./data/products');
+
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Order = require('./models/Order');
+
+require('dotenv').config();
+connectDB();
+
+const importData = async () =>{
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        const createUsers = await User.insertMany(users)
+
+        const adminUser = createUsers[0]._id
+
+        const sampleProducts = products.map(product=>{
+            return {...product,user:adminUser}
+        })
+        await Product.insertMany(sampleProducts)
+        console.log('Data Imported ...');
+        process.exit()
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+const destroyData = async () =>{
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        console.log('Data Destroyed ...');
+        process.exit()
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+if(process.argv[2]==='-d'){
+    destroyData()
+}
+else{
+    importData()
+}
