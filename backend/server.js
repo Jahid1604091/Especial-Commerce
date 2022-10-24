8f4beb6fa764d35f3418912feed930ae87035d7c
Jahid1604091 <jh409780@gmail.com>
2022-10-24 07:17:49 +0600

    model created

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 const products = require('./data/products');
-
-app.get('/api/products',(req,res)=>{
+require('dotenv').config();
+const connectDB = require('./config/db');
+connectDB();
+app.get('/api/products', (req, res) => {
     res.json(products)
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}...`);
+app.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`);
 })
