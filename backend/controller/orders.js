e43cec0315f0ea69121785a0aed8e513c4719daf
Jahid1604091 <jh409780@gmail.com>
2022-11-12 10:35:42 +0600

    update order to paid endpoint

--- backend/controller/orders.js
+++ backend/controller/orders.js
@@ -6,7 +6,7 @@ const Order = require("../models/Order");
 //@desc     post create a new order
 //@access   protected
 exports.createOrder = asyncHandler(async (req, res) => {
-    const order = new Order({...req.body, user:req.user._id})
+    const order = new Order({ ...req.body, user: req.user._id })
     const newOrder = await order.save()
     return res.status(200).json({
         success: true,
@@ -18,7 +18,7 @@ exports.createOrder = asyncHandler(async (req, res) => {
 //@desc     GET get my orders
 //@access   protected
 exports.getMyOrders = asyncHandler(async (req, res) => {
-    const orders = await Order.find({user:req.user._id})
+    const orders = await Order.find({ user: req.user._id })
     return res.status(200).json({
         success: true,
         data: orders
@@ -29,10 +29,31 @@ exports.getMyOrders = asyncHandler(async (req, res) => {
 //@desc     GET get one of my order details
 //@access   protected
 exports.getMyOrder = asyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate('user','id name email')
+    const order = await Order.findById(req.params.id).populate('user', 'id name email')
     return res.status(200).json({
         success: true,
         data: order
+    });
+})
+
+//@route    /api/orders/myorders/:id/pay
+//@desc     put update order to paid
+//@access   protected
+exports.updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+        id:req.body.id,
+        status:req.body.status,
+        update_time:req.body.update_time,
+        email_address:req.body.payer.email_address,
+    }
+
+    const updatedOrder = await order.save();
+    return res.status(200).json({
+        success: true,
+        data: updatedOrder
     });
 })
 
@@ -40,7 +61,7 @@ exports.getMyOrder = asyncHandler(async (req, res) => {
 //@desc     GET all  orders
 //@access   protected/Admin
 exports.getAllOrders = asyncHandler(async (req, res) => {
-    const orders = await Order.find({}).populate('user','id name')
+    const orders = await Order.find({}).populate('user', 'id name')
     return res.status(200).json({
         success: true,
         data: orders
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { createOrder, getAllOrders, getMyOrders, getMyOrder } = require('../controller/orders');
+const { createOrder, getAllOrders, getMyOrders, getMyOrder, updateOrderToPaid } = require('../controller/orders');
 const { protect, authorize } = require('../middleware/auth');
 
 router.route('/').post(protect, createOrder)
@@ -9,8 +9,7 @@ router.route('/').post(protect, createOrder)
 
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/myorders/:id').get(protect, getMyOrder)
-
-
+router.route('/myorders/:id/pay').get(protect, updateOrderToPaid)
 
 
 module.exports = router;
